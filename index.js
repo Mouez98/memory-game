729a2a624c00b2f6ef7bda35aca80d3ff24a3f2e
Mouez98 <mouez.matoui@gmail.com>
2022-07-07 19:00:19 +0100

    adding scores

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-// Variables
+/// Variables ///
 const startBtn = document.querySelector('.modal button'),
   userName = document.querySelector('.user span'),
   userTries = document.querySelector('.wrong-tries span'),
@@ -10,53 +9,101 @@ const startBtn = document.querySelector('.modal button'),
   bgSound = document.getElementById('bg'),
   loseSound = document.getElementById('lose'),
   domTimer = document.querySelector('.timer'),
-  difficultyBtns =  document.querySelectorAll('.game-difficulty li');
+  difficultyBtns = document.querySelectorAll('.game-difficulty li'),
+  volumeBtns = document.querySelectorAll('.volume img'),
+  modal = document.querySelector('.modal');
 
+  const highestScore = {
+    name : 'Mouez',
+    wrongTries: 1,
+    time: 25
+  }
 
-// startBtn.parentElement.parentElement.remove()
+  const winActions = () => {
+    alert('Congratulations you made it')
+    userTries.textContent = 0;
+    document.querySelector('.name').textContent = highestScore.name
+    document.querySelector('.tries').textContent = highestScore.wrongTries
+    document.querySelector('.time').textContent = highestScore.time
+  }
 
-// Events
+  const loseActions = () => {
+    alert('Ops! failed try again')
+        userTries.textContent = 0;
+  }
+
+/// Events ///
 startBtn.addEventListener('click', startBtnHandler);
 
+// Sound btns events
+volumeBtns.forEach((volume) =>
+  volume.addEventListener('click', bgSoundHandler)
+);
 // Shuffle cards & Add events
 cards.forEach((card, index) => {
+
   // Change Cards orders
   shufle(orderRange);
   card.style.order = orderRange[index];
-
+  
+  // Card events
   card.addEventListener('click', (e) => {
     const currentCard = e.target;
-    
-    flipCard(currentCard)
-  
-  })
-})
+    const correctCards = [...cards].filter(card => card.classList.contains('has-match'))
+     console.log(correctCards.length);
+     console.log(cards.length);
+    if(correctCards.length === cards.length) {
+      alert('Congratulations you made it')
+      userTries.textContent = 0
+    } 
+
+    flipCard(currentCard);
+  });
+});
 
 //Set Game difficulty
-difficultyBtns.forEach((level)=> level.addEventListener('click', (e)=> {
-  
-  if(e.target.dataset.difficulty === 'easy') {
-    domTimer.setAttribute('data-time', 60000)
-  }
-  if(e.target.dataset.difficulty === 'medium') {
-    domTimer.setAttribute('data-time', 45000)
-  }
-  if(e.target.dataset.difficulty === 'hard') {
-    domTimer.setAttribute('data-time', 25000)
-  }
-  startBtnHandler()
-}))
+difficultyBtns.forEach((level) =>
+  level.addEventListener('click', gameDifficulty)
+);
 
-// Functions
+/// Functions ///
 
-//Valid number on the DOM
+// Start game with chosen difficulty
+function gameDifficulty (e) {
+  if (e.target.dataset.difficulty === 'easy') {
+    domTimer.setAttribute('data-time', 60000);
+  }
+  if (e.target.dataset.difficulty === 'medium') {
+    domTimer.setAttribute('data-time', 45000);
+  }
+  if (e.target.dataset.difficulty === 'hard') {
+    domTimer.setAttribute('data-time', 25000);
+  }
+  startBtnHandler();
+}
+
+function bgSoundHandler(e) {
+  //Check which icon is displayed and change bgSound state
+    if (e.target.classList.contains('sound-on')) {
+      e.target.classList.add('d-none');
+      e.target.nextElementSibling.classList.remove('d-none');
+      bgSound.pause();
+    }
+    if (e.target.classList.contains('sound-off')) {
+      e.target.classList.add('d-none');
+      e.target.previousElementSibling.classList.remove('d-none');
+      bgSound.play();
+    }
+}
+
+// Valid number on the DOM
 function validNum(time, domElement) {
   domElement.textContent = `${
-    time < 10 && time >= 0 ? `0${time}` : time < 0 ? "00" : time
+    time < 10 && time >= 0 ? `0${time}` : time < 0 ? '00' : time
   }`;
 }
 
-// Timer 
+// Timer
 function timer(time) {
   // Time Goal
   const timeGoal = new Date().getTime() + time;
@@ -64,7 +111,7 @@ function timer(time) {
   const counter = setInterval(() => {
     // Get current Time
     const currentTime = new Date().getTime();
-    
+
     // Get time different
     const timeDifferent = timeGoal - currentTime;
 
@@ -73,113 +120,109 @@ function timer(time) {
     const seconds = Math.floor((timeDifferent % (1000 * 60)) / 1000);
 
     // Get DOM placeholders
-   const domMinutes = document.querySelector('.timer .minutes'),
-    domSeconds = document.querySelector('.timer .seconds');
+    const domMinutes = document.querySelector('.timer .minutes'),
+      domSeconds = document.querySelector('.timer .seconds');
 
-    validNum(minutes, domMinutes)
-    validNum(seconds, domSeconds)
-   
-    
+    validNum(minutes, domMinutes);
+    validNum(seconds, domSeconds);
+
     if (timeDifferent < 0) {
-      clearInterval(counter)
-    alert('time is over')
-    bgSound.pause()
+      clearInterval(counter);
+      const correctCards = [...cards].filter(card => card.classList.contains('has-match'))
+      if(correctCards.length === cards.length) {
+         winActions()
 
-    // Change Cards orders
-    cards.forEach((card, index )=> {
-      shufle(orderRange);
-  card.style.order = orderRange[index];
-  card.classList.remove('has-match')
-  card.classList.remove('flip')
-    })
-  
-    };
-  }, 1000);
+      } else {
+        winActions()
+      }
 
+      bgSound.pause();
+
+      // Change Cards orders
+      cards.forEach((card, index) => {
+        shufle(orderRange);
+        card.style.order = orderRange[index];
+        card.classList.remove('has-match');
+        card.classList.remove('flip');
+      });
+    }
+  }, 500);
 }
 
 // Flip card
 function flipCard(selectedCard) {
-
   // add a flip class to the selected card
-  selectedCard.classList.add('flip')
+  selectedCard.classList.add('flip');
 
   //Collect Flipped cards
-  let allFlippedCards =  [...cards].filter(flippedCard => flippedCard.classList.contains('flip'))
+  let allFlippedCards = [...cards].filter((flippedCard) =>
+    flippedCard.classList.contains('flip')
+  );
 
-  if(allFlippedCards.length === 2) {
-    checkMatchedCard(allFlippedCards[1], allFlippedCards[0])
-   
+  if (allFlippedCards.length === 2) {
+    checkMatchedCard(allFlippedCards[1], allFlippedCards[0]);
+
     //Stop Clicking
-      stopClicking()
-
-    
-   }
-
+    stopClicking();
+  }
 }
 
 //Stop Clicking Function
 function stopClicking() {
-
   // Add class no clicking on cradsContainer
-    cardsContainer.classList.add('no-click') 
+  cardsContainer.classList.add('no-click');
 
-    setTimeout(() => {
-
-      // Remove class no clicking on cradsContainer
-      cardsContainer.classList.remove('no-click')
-    }, 1000)
+  setTimeout(() => {
+    // Remove class no clicking on cradsContainer
+    cardsContainer.classList.remove('no-click');
+  }, 1000);
 }
 
-// Check matched Cards 
-function checkMatchedCard(firstCard, secondCard){
-    
-  if(firstCard.dataset.tech === secondCard.dataset.tech) {
-      firstCard.classList.remove('flip')
-      secondCard.classList.remove('flip')
-      
-      firstCard.classList.add('has-match')
-      secondCard.classList.add('has-match')
-      succesSound.play()
+// Check matched Cards
+function checkMatchedCard(firstCard, secondCard) {
+  if (firstCard.dataset.tech === secondCard.dataset.tech) {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    firstCard.classList.add('has-match');
+    secondCard.classList.add('has-match');
+    succesSound.play();
   } else {
     userTries.innerText = parseInt(userTries.innerText) + 1;
-    userTries.parentElement.classList.add('text-danger')
-    loseSound.play()
+
+    loseSound.play();
     setTimeout(() => {
-      firstCard.classList.remove('flip')
-      secondCard.classList.remove('flip')
-    }, 1000)
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+    }, 500);
   }
 }
 
 // Start Function
 function startBtnHandler() {
-  const modal = document.querySelector('.modal');
+  
 
-  // Remove modal from DOM
-  modal.remove();
+  // Hide modal
+  modal.classList.add('d-none')
 
   // Get user input
   let yourName = prompt('enter your name');
-  
+
   changeUserName(yourName);
 }
 
 // Change user name
 function changeUserName(name) {
-
-  // Set Timer 
-  const time = domTimer.dataset.time * 1
-  timer(time)
+  // Set Timer
+  const time = domTimer.dataset.time * 1;
+  timer(time);
 
   const verfiyName =
     name === '' || !name ? `Guest${Math.trunc(Math.random() * 1010)}` : name;
   userName.textContent = verfiyName;
 
   // Add background sound
-  bgSound.play()
-
-  
+  bgSound.play();
 }
 
 // Shuffle Function
